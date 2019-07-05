5894a37ded2e7f05a92727981de7e44bf5e17a10
Rahul Yedida <yrahul3910@gmail.com>
2019-07-06 01:06:47 +0530

    ListView

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,44 @@
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     Stage window;
-    ComboBox<String> comboBox;
+    ListView<String> listView;
 
     @Override
     public void start(Stage stage) throws Exception {
         window = stage;
         window.setTitle("Title");
 
-        comboBox = new ComboBox<>();
-        comboBox.getItems().addAll("RED", "1989", "reputation", "Speak Now", "Lover", "Taylor Swift", "Fearless");
-        comboBox.setPromptText("Favorite Taylor Swift album");
-        comboBox.setOnAction(e -> printAlbum());
-        comboBox.setEditable(true);  // overwrites prompt text
+        listView = new ListView<>();
+        listView.getItems().addAll("RED", "1989", "reputation", "Lover", "Fearless", "Speak Now", "Taylor Swift");
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-        Button button = new Button("Useless button");
-        button.setOnAction(e -> printAlbum());
+        Button button = new Button("Submit");
+        button.setOnAction(e -> printItems());
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
-        layout.getChildren().addAll(comboBox, button);
+        layout.getChildren().addAll(listView, button);
 
         Scene scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
     }
 
-    private void printAlbum() {
-        System.out.println(comboBox.getValue());
+    private void printItems() {
+        ObservableList<String> selected = listView.getSelectionModel().getSelectedItems();
+
+        for (String s : selected)
+            System.out.println(s);
     }
 
     public static void main(String[] args) {
