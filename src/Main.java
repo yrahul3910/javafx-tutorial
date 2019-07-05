0eb0ab327c1b57c1eae4f26f8e9a42553dc675cf
Rahul Yedida <yrahul3910@gmail.com>
2019-07-05 22:39:35 +0530

    Embedding layouts

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,40 @@
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     Stage window;
-    Scene scene1, scene2;
 
     @Override
     public void start(Stage stage) throws Exception {
         window = stage;
         window.setTitle("Title");
 
-        window.setOnCloseRequest(e -> closeProgram());
+        HBox topMenu = new HBox();
+        Button buttonA = new Button("File");
+        Button buttonB = new Button("Edit");
+        Button buttonC = new Button("View");
+        topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
 
-        Label label1 = new Label("Welcome to scene 1!");
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction(e -> window.setScene(scene2));
+        VBox leftMenu = new VBox();
+        Button buttonD = new Button("File");
+        Button buttonE = new Button("Edit");
+        Button buttonF = new Button("View");
+        leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
 
-        Button closeButton = new Button("Close program");
-        closeButton.setOnAction(e -> {
-            e.consume(); // We'll consume the event, so we're going to handle the close event
-            closeProgram();
-        });
+        BorderPane layout = new BorderPane();
+        layout.setTop(topMenu);
+        layout.setLeft(leftMenu);
 
-        // Layout 1
-        VBox layout1 = new VBox(20); // 20 is the space between elements
-        layout1.getChildren().addAll(label1, button1, closeButton);
-        scene1 = new Scene(layout1, 200, 200);
+        Scene scene = new Scene(layout, 300, 250);
 
-        Button button2 = new Button("Go to scene 1");
-        Button button3 = new Button("Show alert box");
-        button2.setOnAction(e -> window.setScene(scene1));
-        button3.setOnAction(e -> {
-            boolean result = ConfirmBox.display("Modal title", "Are you sure?");
-            System.out.println(result);
-        });
-
-        // Layout 2
-        VBox layout2 = new VBox(20);
-        layout2.getChildren().addAll(button2, button3);
-        scene2 = new Scene(layout2, 600, 300);
-
-        window.setScene(scene1);
-        window.setTitle("Title");
+        window.setScene(scene);
         window.show();
-    }
-
-    private void closeProgram() {
-        boolean close = ConfirmBox.display("Confirm exit", "Are you sure you want to exit?");
-        if (close)
-            window.close();
     }
 
     public static void main(String[] args) {
